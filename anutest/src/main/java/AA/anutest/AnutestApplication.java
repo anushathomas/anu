81ff96b00153be12e67f884ce2fabb2c30b9f134
Thomas <anusha.thomas@capgemini.com>
2018-08-29 12:21:35 +0530

    first commit

---
+++ anutest/src/main/java/AA/anutest/AnutestApplication.java
@@ -0,0 +1,12 @@
+package AA.anutest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnutestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnutestApplication.class, args);
+	}
+}
